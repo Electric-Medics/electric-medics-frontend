2b37327012ede17c7c7b6c5403380152459c2386
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-02-19 01:06:34 -0800

    (init)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Container } from '@material-ui/core';
 import './App.css';
 
-import About from './components/About';
-import Home from './components/Home';
-import Reviews from './components/Reviews';
-import Services from './components/Services';
-import Showroom from './components/Showroom';
+import About from './views/About';
+import Home from './views/Home';
+import MenuBar from './components/MenuBar';
+import Reviews from './views/Reviews';
+import Services from './views/Services';
+import Showroom from './views/Showroom';
 
 const App = () => {
   return (
     <Router>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/about' component={About} />
-      <Route exact path='/services' component={Services} />
-      <Route exact path='/reviews' component={Reviews} />
-      <Route exact path='/showroom' component={Showroom} />
+      <Container maxWidth='xl'>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/services' component={Services} />
+        <Route exact path='/reviews' component={Reviews} />
+        <Route exact path='/showroom' component={Showroom} />
+      </Container>
     </Router>
   );
 };
