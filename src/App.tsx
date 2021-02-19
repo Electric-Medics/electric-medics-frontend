c29a14c4f4c02e3c583baa53d8b7c7ce536ed086
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-02-19 12:03:27 -0800

    Initialize a new branch for terraform pull request

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from '@material-ui/core';
+import React from "react";
 import './App.css';
-
-import MenuBar from './components/MenuBar';
-
-import About from './views/About';
-import Home from './views/Home';
-import Reviews from './views/Reviews';
-import Services from './views/Services';
-import Showroom from './views/Showroom';
 
 const App = () => {
   return (
-    <Router>
-      <Container maxWidth='xl'>
-        <MenuBar />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/services' component={Services} />
-        <Route exact path='/reviews' component={Reviews} />
-        <Route exact path='/showroom' component={Showroom} />
-      </Container>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+\        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 };
 
--- src/views/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MainCarousel from "../components/MainCarousel";
-
-const Home = () => {
-  return <MainCarousel />;
-};
-
-export default Home;
