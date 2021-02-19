c0900335d49c83d920eeb9b7e07d09ff82f17e77
dylvaz <vazquezadylan@gmail.com>
2021-02-18 22:04:48 -0800

    (init)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
+
+import About from './components/About';
+import Home from './components/Home';
+import Reviews from './components/Reviews';
+import Services from './components/Services';
+import Showroom from './components/Showroom';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>clap cheeks 👏🏿</h1>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/services' component={Services} />
+      <Route exact path='/reviews' component={Reviews} />
+      <Route exact path='/showroom' component={Showroom} />
+    </Router>
   );
 };
 
