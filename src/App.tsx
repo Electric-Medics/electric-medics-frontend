973161a13d13d510c3af36e092fa9a2271de1b5c
dylvaz <vazquezadylan@gmail.com>
2021-02-19 21:00:08 -0800

    implemented Career route and removed maxwidth from container

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Container } from '@material-ui/core';
 import './App.css';
 
 import About from './views/About';
+import Careers from './views/Careers';
 import Home from './views/Home';
 import MenuBar from './components/MenuBar';
 import Reviews from './views/Reviews';
@@ -13,13 +14,14 @@ import Showroom from './views/Showroom';
 const App = () => {
   return (
     <Router>
-      <Container maxWidth='xl'>
+      <Container maxWidth={false}>
         <MenuBar />
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
         <Route exact path='/services' component={Services} />
         <Route exact path='/reviews' component={Reviews} />
         <Route exact path='/showroom' component={Showroom} />
+        <Route exact path='/careers' component={Careers} />
       </Container>
     </Router>
   );
