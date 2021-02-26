5cbf3c27cb87a406b6ae42a6dd7735ec822ca631
dylvaz <vazquezadylan@gmail.com>
2021-02-25 23:46:51 -0800

    implemented classNames to remove hover effects and added extra styles :crayon:
    - added different fontweights and sizes for the header info

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,11 @@ const useStyles = makeStyles({
     position: 'relative',
     top: '20px',
     fontFamily: 'Work Sans, sans-serif',
-    fontWeight: 300,
+    fontWeight: 400,
+  },
+  gridItemTitle: {
+    fontWeight: 500,
+    fontSize: '18px',
   },
   gridItemContainer: {
     display: 'flex',
@@ -28,7 +32,7 @@ const Header = () => {
   return (
     <Grid container className={classes.root}>
       <Grid item xs={3}>
-        <Button href='/'>
+        <Button href='/' className='headerLogo'>
           <img
             src={Logo}
             alt='Electrics Medics Logo'
@@ -42,21 +46,23 @@ const Header = () => {
         </Button>
       </Grid>
       <Grid item xs={3} style={{ top: '20px' }}>
-        <p>The Best Electricians</p>
+        <p className={classes.gridItemTitle}>The Best Electricians</p>
         <p>Serving Mission Viejo and Surrounding Areas</p>
       </Grid>
       <Grid item xs={2} style={{ top: '20px' }}>
-        <p>BUISNESS HOURS:</p> <p>Monday - Friday: 8:00AM - 5:00PM</p>
+        <p className={classes.gridItemTitle}>BUSINESS HOURS:</p>
+        <p>Monday - Friday: 8:00AM - 5:00PM</p>
       </Grid>
       <Grid className={classes.gridItemContainer} item xs={3} justify='center'>
         <span className='containerSubmitRequest'>
           <span className='btnSubmitRequest'>
-            <span>Submit A Request</span>
+            <span>SUBMIT REQUEST</span>
           </span>
         </span>
         <Button
           className={classes.mapMarker}
           href='https://www.google.com/maps/place/Electric+Medics/@33.613528,-117.6789378,15z/data=!4m5!3m4!1s0x0:0xbfe07d4931a23eaf!8m2!3d33.613528!4d-117.6789378'
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0) !important' }}
         >
           <Icon path={mdiMapMarker} size={2} color='#1A8FE3' />
         </Button>
