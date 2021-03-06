91b464aa71472af486242bc7eb3f7b793a3868d7
dylvaz <vazquezadylan@gmail.com>
2021-03-05 20:48:16 -0800

    init :zap:

---
+++ src/components/HomeReviews.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeReviews = () => {
+  return (
+    <div style={{ margin: '20px', padding: '20px' }}>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default HomeReviews;
