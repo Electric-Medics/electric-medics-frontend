cb3e5834f0f78f74445f4b65af4717cc6be29b23
dylvaz <vazquezadylan@gmail.com>
2021-03-08 23:59:40 -0800

    added rendering of HomeReviews

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Carousel from '../components/Carousel/Carousel';
+import HomeReviews from '../components/HomeReviews';
 
 const Home = () => {
   return (
     <Grid container>
       <Grid item xs={12}>
         <Carousel />
+        <HomeReviews />
       </Grid>
     </Grid>
   );
