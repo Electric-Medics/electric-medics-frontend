87e3cb88b0531276bd196f29c82a4743e83bb7ec
dylvaz <vazquezadylan@gmail.com>
2021-03-09 03:15:48 -0800

    updated import statement

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
 import Carousel from '../components/Carousel/Carousel';
 import HomeReviews from '../components/HomeReviews/HomeReviews';
 import AboutUs from '../components/About/AboutUs';
+import RequestForm from '../components/RequestForm/RequestForm';
 
 const Home = () => {
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Carousel />
-        <AboutUs />
-        <HomeReviews />
-      </Grid>
-    </Grid>
+    <>
+      <Carousel />
+      <AboutUs />
+      <HomeReviews />
+      <RequestForm />
+    </>
   );
 };
 
