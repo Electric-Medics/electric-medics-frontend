532fa74a7e1063a11fa75307768507c3a41f9c4a
dylvaz <vazquezadylan@gmail.com>
2021-03-09 00:26:11 -0800

    updated import implemented the useHistory hook from react ro

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Carousel from '../components/Carousel/Carousel';
-import HomeReviews from '../components/HomeReviews';
+import HomeReviews from '../components/HomeReviews/HomeReviews';
 import AboutUs from '../components/About/AboutUs';
 
 const Home = () => {
