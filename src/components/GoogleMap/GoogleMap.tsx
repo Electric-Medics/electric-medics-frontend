2fc30bf767a0c1430eb23a75d20f45481a85d979
dylvaz <vazquezadylan@gmail.com>
2021-03-10 15:06:43 -0800

    added process.env variables for key

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import GoogleMapReact from 'google-map-react';
+import * as dotenv from 'dotenv';
 
 import './GoogleMap.css';
 import LocationPin from '../LocationPin/LocationPin';
+
+dotenv.config();
 
 interface Location {
   address: string;
@@ -19,11 +22,13 @@ const GoogleMap: FunctionComponent<GoogleMapProps> = ({
   location,
   zoomLevel,
 }: GoogleMapProps) => {
+  let key: string = process.env.REACT_APP_GOOGLE_MAP_API_KEY ?? '';
+
   return (
     <div className='map'>
       <div className='google-map'>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCK_sufXJt1gXyEwu1oLAmDq6YqJb5pKQo' }}
+          bootstrapURLKeys={{ key }}
           defaultCenter={location}
           defaultZoom={zoomLevel}
         >
