0537bcf310e4e658a9e0cef5d2425406b02911e6
dylvaz <vazquezadylan@gmail.com>
2021-03-10 14:36:17 -0800

    implemented a GoogleMap component that renders a google map using Googles api

---
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import './GoogleMap.css';
+import LocationPin from '../LocationPin/LocationPin';
+
+interface Location {
+  address: string;
+  lat: number;
+  lng: number;
+}
+
+type GoogleMapProps = {
+  location: Location;
+  zoomLevel: number;
+};
+
+const GoogleMap: FunctionComponent<GoogleMapProps> = ({
+  location,
+  zoomLevel,
+}: GoogleMapProps) => {
+  const handleApiLoaded = (map, maps) => {
+    console.log(map, maps);
+  };
+  return (
+    <div className='map'>
+      <div className='google-map'>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCK_sufXJt1gXyEwu1oLAmDq6YqJb5pKQo' }}
+          defaultCenter={location}
+          defaultZoom={zoomLevel}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        >
+          <LocationPin lat={location.lat} lng={location.lng} />
+        </GoogleMapReact>
+      </div>
+    </div>
+  );
+};
+
+export default GoogleMap;
