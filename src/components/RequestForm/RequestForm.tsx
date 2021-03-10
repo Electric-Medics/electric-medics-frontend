5ca61000cfb23b4b4efefa93c3532d0f98198f83
dylvaz <vazquezadylan@gmail.com>
2021-03-09 16:18:28 -0800

    implemented placeHolders that function as labels, sanitized input, fixed errors
    * implemented custom error messages for email and phonenumber
    * cleaned up input during onSubmit by trimming white space

--- src/components/RequestForm/RequestForm.tsx
+++ src/components/RequestForm/RequestForm.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-import FormBackground from '../../assets/images/formBackground.jpg';
 import RequestFormMap from '../../assets/images/requestFormMap.png';
 import './RequestForm.css';
 
@@ -15,51 +14,62 @@ type Inputs = {
 
 const RequestForm = () => {
   const { register, handleSubmit, errors } = useForm<Inputs>();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = (data) => {
+    for (const key in data) {
+      data[key] = data[key].trim();
+    }
+  };
+
   return (
-    <div
-      className='container-fluid py-4'
-      style={{
-        background: `url(${FormBackground}) no-repeat center center /cover`,
-      }}
-    >
-      <div className='row'>
-        <div className='col-6 mt-5 ms-5'>
+    <div className='container-fluid pt-4'>
+      <div className='row' style={{ maxHeight: '650px' }}>
+        <div className='col-6 mt-5'>
           <a href='https://www.google.com/maps/place/Electric+Medics/@33.613528,-117.6789378,15z/data=!4m5!3m4!1s0x0:0xbfe07d4931a23eaf!8m2!3d33.613528!4d-117.6789378'>
             <img
               src={RequestFormMap}
               alt='Map of southern Orange County'
               style={{
-                width: '90%',
-                height: '85%',
-                borderRadius: '50px',
+                width: '100%',
+                height: '93%',
                 border: '2px solid rgb(241, 136, 5)',
+                maxHeight: '600px',
               }}
             />
           </a>
         </div>
         <div className='col-5 mt-5'>
           <h1
-            className='text-center'
+            className='text-center mb-3'
             style={{
               fontFamily: 'Work Sans, sans-serif',
-              fontWeight: 600,
-              color: 'white',
+              fontWeight: 400,
+              color: 'black',
+              maxHeight: '600px',
             }}
           >
             Submit a Request
           </h1>
-          <div className='container-fluid' style={{ color: 'white' }}>
+          <div
+            className='container-fluid'
+            style={{
+              color: 'black',
+              fontFamily: 'Work Sans, sans-serif',
+            }}
+          >
             <div>
-              <form className='row g-3' onSubmit={handleSubmit(onSubmit)}>
-                <div className='col-md-6'>
-                  <label htmlFor='inputFullName' className='form-label'>
-                    Full Name
-                  </label>
+              <form
+                noValidate
+                className='row g-3'
+                onSubmit={handleSubmit(onSubmit)}
+                style={{ minHeight: '500px' }}
+              >
+                <div className='col-md-6 mb-3'>
                   <input
                     type='text'
                     name='name'
                     className='form-control'
+                    placeholder='Enter your full name'
                     id='inputFullName'
                     ref={register({
                       required: true,
@@ -70,83 +80,87 @@ const RequestForm = () => {
                     <span className='error-msg'>This field is required</span>
                   )}
                 </div>
-                <div className='col-md-6'>
-                  <label htmlFor='email' className='form-label'>
-                    Email
-                  </label>
+                <div className='col-md-6 mb-3'>
                   <input
                     type='email'
                     name='email'
                     className='form-control'
                     id='inputEmail'
-                    placeholder='johndoe@email.com'
+                    placeholder='Enter your email'
                     ref={register({
                       required: true,
                       pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                     })}
                   />
-                  {errors.email && (
-                    <span className='error-msg'>This field is required</span>
-                  )}
+                  {errors.email ? (
+                    errors.email.type === 'pattern' ? (
+                      <span className='error-msg'>
+                        Please enter a valid email.
+                      </span>
+                    ) : (
+                      <span className='error-msg'>This field is required</span>
+                    )
+                  ) : null}
                 </div>
-                <div className='col-12'>
-                  <label htmlFor='phoneNumber' className='form-label'>
-                    Phone Number
-                  </label>
+                <div className='col-12 mb-3'>
                   <input
                     type='tel'
                     name='phoneNumber'
                     className='form-control'
                     id='inputPhoneNumber'
-                    placeholder='949-'
+                    placeholder='Enter your phone number'
                     ref={register({
                       required: true,
-                      pattern: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/,
+                      pattern: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
                     })}
                   />
-                  {errors.phoneNumber && (
-                    <span className='error-msg'>This field is required</span>
-                  )}
+                  {errors.phoneNumber ? (
+                    errors.phoneNumber.type === 'pattern' ? (
+                      <span className='error-msg'>
+                        Please enter a valid phone number.
+                      </span>
+                    ) : (
+                      <span className='error-msg'>This field is required</span>
+                    )
+                  ) : null}
                 </div>
-                <div className='col-12'>
-                  <label htmlFor='city' className='form-label'>
-                    City
-                  </label>
+                <div className='col-12 mb-3'>
                   <input
                     type='text'
                     className='form-control'
                     id='city'
                     name='city'
+                    placeholder='Enter your city'
                     ref={register({ required: true, maxLength: 70 })}
                   />
                   {errors.city && (
                     <span className='error-msg'>This field is required</span>
                   )}
                 </div>
-                <div className='col-12'>
-                  <label htmlFor='request' className='form-label'>
-                    How can we help you?
-                  </label>
+                <div className='col-12 mb-3'>
                   <textarea
                     className='form-control'
                     name='request'
                     id='request'
+                    placeholder='How can we help you?'
+                    style={{ height: '150px' }}
                     ref={register({ required: true, maxLength: 500 })}
                   />
                   {errors.request && (
                     <span className='error-msg'>This field is required</span>
                   )}
                 </div>
-                <div className='col-12'>
+                <div className='col-12 d-flex justify-content-center'>
                   <button
                     type='submit'
                     className='btn btn-lg'
                     style={{
                       backgroundColor: 'rgb(241, 136, 5)',
                       borderColor: 'rgb(241, 136, 5)',
+                      width: '70%',
                     }}
                   >
-                    Submit
+                    Submit your inquiry
                   </button>
                 </div>
               </form>
