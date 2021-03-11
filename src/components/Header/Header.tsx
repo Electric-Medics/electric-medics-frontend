788b33c2c0ab3a0c406772f3e8df2b59aebf1482
dylvaz <vazquezadylan@gmail.com>
2021-03-10 20:00:39 -0800

    implemented refactored header - no longer needs MUI and now uses bootstrap for code consistency - same functionality as before

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,73 +1,58 @@
 import React from 'react';
-import { Button, Grid, makeStyles } from '@material-ui/core';
-import { mdiMapMarker } from '@mdi/js';
-import Icon from '@mdi/react';
+import { useHistory } from 'react-router';
 
-import './Header.css';
 import Logo from '../../assets/images/transparentLogo.png';
-
-const useStyles = makeStyles({
-  root: {
-    position: 'relative',
-    top: '20px',
-    fontFamily: 'Work Sans, sans-serif',
-    fontWeight: 400,
-  },
-  gridItemTitle: {
-    fontWeight: 500,
-    fontSize: '18px',
-  },
-  gridItemContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  mapMarker: {
-    top: '-17px',
-    marginLeft: '10px',
-  },
-});
+import './Header.css';
 
 const Header = () => {
-  const classes = useStyles();
-
+  let history = useHistory();
   return (
-    <Grid container justify='flex-start' className={classes.root}>
-      <Grid item xs={3}>
-        <Button href='/' className='headerLogo'>
-          <img
-            src={Logo}
-            alt='Electrics Medics Logo'
-            style={{
-              position: 'relative',
-              height: '125px',
-              width: '225px',
-              top: '-25px',
-            }}
-          />
-        </Button>
-      </Grid>
-      <Grid item xs={3} style={{ top: '20px' }}>
-        <p className={classes.gridItemTitle}>The Best Electricians</p>
-        <p>Serving Mission Viejo and Surrounding Areas</p>
-      </Grid>
-      <Grid item xs={2} style={{ top: '20px' }}>
-        <p className={classes.gridItemTitle}>BUSINESS HOURS:</p>
-        <p>Monday - Friday: 8:00AM - 5:00PM</p>
-      </Grid>
-      <Grid className={classes.gridItemContainer} item xs={3}>
-        <span className='containerSubmitRequest'>
-          <span className='btnSubmitRequest'>
-            <span>SUBMIT REQUEST</span>
+    <div className='container-fluid pt-3'>
+      <div className='row' style={{ maxHeight: '137px' }}>
+        <div
+          className='col-3 d-flex justify-content-center p-0 logo'
+          style={{
+            background: `url(${Logo})`,
+            backgroundSize: '90%',
+            backgroundRepeat: 'no-repeat',
+            height: '137px',
+            maxHeight: '137px',
+            color: 'white',
+            marginLeft: '25px',
+            maxWidth: '260px',
+            marginRight: '30px',
+          }}
+          onClick={() => {
+            history.push('/');
+          }}
+        ></div>
+        <div className='col-3 pt-2'>
+          <p className='header-title'>The Best Electricians</p>
+          <p className='header-text'>
+            Serving Mission Viejo and Surrounding Areas
+          </p>
+        </div>
+        <div className='col-3 pt-2'>
+          <p className='header-title'>BUSINESS HOURS:</p>
+          <p className='header-text'>Monday - Friday: 8:00AM - 5:00PM</p>
+        </div>
+        <div className='col-2 pt-3 align-self-start'>
+          <span className='containerSubmitRequest'>
+            <span className='btnSubmitRequest'>
+              <span>SUBMIT REQUEST</span>
+            </span>
           </span>
-        </span>
-        <Button
-          className={classes.mapMarker}
-          href='https://www.google.com/maps/place/Electric+Medics/@33.613528,-117.6789378,15z/data=!4m5!3m4!1s0x0:0xbfe07d4931a23eaf!8m2!3d33.613528!4d-117.6789378'
-        >
-          <Icon path={mdiMapMarker} size={2} color='#1A8FE3' />
-        </Button>
-      </Grid>
-    </Grid>
+        </div>
+        <div className='col-1'>
+          <a href='https://www.google.com/maps/place/Electric+Medics/@33.613528,-117.6789378,15z/data=!4m5!3m4!1s0x0:0xbfe07d4931a23eaf!8m2!3d33.613528!4d-117.6789378'>
+            <i
+              className='fas fa-map-marker-alt fa-3x'
+              style={{ color: 'rgb(26, 143, 227)', paddingTop: '20px' }}
+            ></i>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
