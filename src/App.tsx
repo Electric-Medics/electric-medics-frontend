8e0b6c5e76606f2e278afa70a7dcb9717f9f1667
dylvaz <vazquezadylan@gmail.com>
2021-03-10 18:52:48 -0800

    implemented the rendering of header and MenuBar2 on every page

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import About from './views/About';
 import Careers from './views/Careers';
 import FadeInSection from './components/FadeInSection/FadeInSection';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Home from './views/Home';
-import MenuBar from './components/MenuBar/MenuBar';
+import MenuBar2 from './components/MenuBar2/MenuBar2';
 import Reviews from './views/Reviews';
 import Services from './views/Services';
 import Showroom from './views/Showroom';
@@ -16,7 +17,8 @@ const App = () => {
   return (
     <Router>
       <div className='contianer-fluid' style={{ padding: 0, margin: 0 }}>
-        <MenuBar />
+        <Header />
+        <MenuBar2 />
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/services' component={Services} />
