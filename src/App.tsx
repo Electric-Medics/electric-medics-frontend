50a5a2c174865d4e274a8ee025b6a36672228109
dylvaz <vazquezadylan@gmail.com>
2021-03-15 21:27:17 -0700

    implemented switch for routes and added NotFoundView.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import About from './views/About';
 import Careers from './views/Careers';
-import FadeInSection from './components/FadeInSection/FadeInSection';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './views/Home';
 import MenuBar from './components/MenuBar/MenuBar';
+import NotFound from './views/NotFound/NotFound';
 import Reviews from './views/Reviews';
 import Services from './views/Services';
 import Showroom from './views/Showroom';
@@ -19,13 +19,15 @@ const App = () => {
       <div className='contianer-fluid' style={{ padding: 0, margin: 0 }}>
         <Header />
         <MenuBar />
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/services' component={Services} />
-        <Route path='/reviews' component={Reviews} />
-        <Route path='/showroom' component={Showroom} />
-        <Route path='/careers' component={Careers} />
-        <FadeInSection />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/services' component={Services} />
+          <Route path='/reviews' component={Reviews} />
+          <Route path='/showroom' component={Showroom} />
+          <Route path='/careers' component={Careers} />
+          <Route path='*' component={NotFound} />
+        </Switch>
         <Footer />
       </div>
     </Router>
